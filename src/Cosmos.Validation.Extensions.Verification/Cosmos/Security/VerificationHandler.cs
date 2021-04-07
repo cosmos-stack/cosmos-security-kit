949ec40b6aade37e211c816ca2a2dce5c1458259
alexinea <forerunner@outlook.com>
2021-04-07 23:56:24 +0800

    Add Validation.Extensions.Verification.

---
+++ src/Cosmos.Validation.Extensions.Verification/Cosmos/Security/VerificationHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Cosmos.Security.Verification;
+using Cosmos.Validation;
+
+namespace Cosmos.Security
+{
+    internal static class VerificationCoreHandler
+    {
+        public static Func<Func<IHashFunction>, Func<object, Func<Encoding, IHashValue>>> Hash()
+            => functionFunc => o => encoding =>
+            {
+                var function = functionFunc();
+                var bytes = VerificationHelper.ConvertToByteArray(o, encoding);
+                return function.ComputeHash(bytes);
+            };
+        public static Func<Func<bool>, Func<string, Func<IHashValue, Func<string, CustomVerifyResult>>>> CompareAndReturn()
+            => compareFunc => hexVal => hashVal => hashName =>
+            {
+                var compareRet = compareFunc();
+                return compareRet
+                    ? new CustomVerifyResult
+                    {
+                        VerifyResult = true
+                    }
+                    : new CustomVerifyResult
+                    {
+                        VerifyResult = false,
+                        ErrorMessage = string.IsNullOrWhiteSpace(hexVal)
+                            ? $"The {hashName} verification result should be {hexVal}, but the actual result is {hashVal.AsHexString()}."
+                            : $"The {hashName} verification result is {hashVal.AsHexString()}, which is not the expected value."
+                    };
+            };
+    }
+}
---
+++ src/Cosmos.Validation.Extensions.Verification/Cosmos/Security/VerificationHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using Cosmos.Optionals;
+using Cosmos.Text;
+
+namespace Cosmos.Security
+{
+    internal static class VerificationHelper
+    {
+        public static byte[] ConvertToByteArray(object obj, Encoding encoding = null)
+        {
+            if (obj is null)
+                return new byte[0];
+
+            encoding = encoding.SafeEncodingValue();
+
+            if (obj is string str)
+                return encoding.GetBytes(str);
+
+            if (obj is byte[] bytes)
+                return bytes;
+
+            //检查 FileInfo， 对 FileInfo 对应的文件进行取样
+            //该功能尚未实现
+
+            return encoding.GetBytes(obj.ToString() ?? string.Empty);
+        }
+
+        public static int Compare(string a, string b, IgnoreCase ignoreCase)
+        {
+            if (ignoreCase == IgnoreCase.FALSE)
+                return string.Compare(a, b, StringComparison.Ordinal);
+            return string.Compare(a, b, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
