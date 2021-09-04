8861d2c7e60d1d8f54c80c5d85e60919e3c2c3b9
AlexLEWIS <liuyis@live.com>
2021-09-04 20:33:24 +0800

    Upadte build scripts.

---
+++ build/Release.bat
@@ -0,0 +1,41 @@
+@echo off
+
+echo =======================================================================
+echo Cosmos.Security.Kits
+echo =======================================================================
+
+::go to parent folder
+cd ..
+
+::create nuget_packages
+if not exist nuget_packages (
+    md nuget_packages
+    echo Created nuget_packages folder.
+)
+
+::clear nuget_packages
+for /R "nuget_packages" %%s in (*) do (
+    del "%%s"
+)
+echo Cleaned up all nuget packages.
+echo.
+
+::start to package all projects
+::Security Kits for Validation
+dotnet pack src/Cosmos.Validation.Extensions.Verification/Cosmos.Validation.Extensions.Verification.csproj -c Release -o nuget_packages --no-restore
+
+for /R "nuget_packages" %%s in (*symbols.nupkg) do (
+    del "%%s"
+)
+
+echo.
+echo.
+
+::push nuget packages to server
+for /R "nuget_packages" %%s in (*.nupkg) do ( 	
+    dotnet nuget push "%%s" -s "Release" --skip-duplicate
+	echo.
+)
+
+::get back to build folder
+cd build
